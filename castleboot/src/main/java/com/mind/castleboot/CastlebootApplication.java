570d4c1b4091d7a9cb8c6593d4e5071f586cf585
castledragon <starlight1532@gmail.com>
2026-06-14 23:39:45 +0900

    init git project

---
+++ castleboot/src/main/java/com/mind/castleboot/CastlebootApplication.java
@@ -0,0 +1,13 @@
+package com.mind.castleboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastlebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastlebootApplication.class, args);
+	}
+
+}
